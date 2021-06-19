79d628640d5886cf485bac0a71250ed32e54edcd
Egor Sheff <egor@MBP-Egor.lan>
2021-06-19 22:18:52 +0300

    add matan and ad

--- wolfram.js
+++ wolfram.js
@@ -35,7 +35,7 @@ function diffRedirect() {
 }
 
 function detParamRedirect(A, res){
-  matStr = "det{"
+  var matStr = "det{"
   for (var i = 0; i < A.length; i++) {
     matStr += "{"
     for (var j = 0; j < A[0].length; j++) {
@@ -45,5 +45,30 @@ function detParamRedirect(A, res){
   }
   matStr = matStr.slice(0, -1) + "}="+res
 
+  window.open(wolframURL+encodeURIComponent(matStr))
+}
+
+function integralRedirect(){
+  var matStr = "integral " + $("#integral").val() + " dx"
+  window.open(wolframURL+encodeURIComponent(matStr))
+}
+
+function definiteIntegralRedirect(){
+  var matStr = "integrate " + $("#definite_integral").val() + " dx, x=" + $("#definite_integral_down").val() + ".." + $("#definite_integral_up").val()
+  window.open(wolframURL+encodeURIComponent(matStr))
+}
+
+function doubleIntegralRedirect(){
+  var matStr = "integrate " + $("#double_integral").val() + " " + $("#double_integral_select").val() + 
+  ", x=" + $("#double_integral_x_down").val() + ".." + $("#double_integral_x_up").val() +
+  ", y=" + $("#double_integral_y_down").val() + ".." + $("#double_integral_y_up").val()
+  window.open(wolframURL+encodeURIComponent(matStr))
+}
+
+function tripleIntegralRedirect(){
+  var matStr = "integrate " + $("#triple_integral").val() + " " + $("#triple_integral_select").val() + 
+  ", x=" + $("#triple_integral_x_down").val() + ".." + $("#triple_integral_x_up").val() +
+  ", y=" + $("#triple_integral_y_down").val() + ".." + $("#triple_integral_y_up").val() +
+  ", z=" + $("#triple_integral_z_down").val() + ".." + $("#triple_integral_z_up").val()
   window.open(wolframURL+encodeURIComponent(matStr))
 }
